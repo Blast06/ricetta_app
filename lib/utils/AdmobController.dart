accea9e23127529e2ca73a00b6fa40f088b9c97d
blast06 <lacalculadora06@hotmail.com>
2022-11-05 20:35:02 -0400

    upgrade and added some packages

--- lib/utils/AdmobController.dart
+++ lib/utils/AdmobController.dart
@@ -1,37 +1,106 @@
 import 'dart:io';
 
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'package:logger/logger.dart';
-import 'package:meta/meta.dart';
-import 'package:native_admob_flutter/native_admob_flutter.dart';
 
-import '../utils/Myadmob.dart';
+import 'MyAdmob.dart';
 
 const TEST = false;
 
 class AdmobController extends GetxController {
   // AdmobInterstitial interstitialAd;
-  late InterstitialAd interstitialAd;
+  // late InterstitialAd interstitialAd;
 
-  late AppOpenAd appOpenAd;
+  // late AppOpenAd appOpenAd;
 
-  final bannerController = BannerAdController();
+  MyAdmob admobConfig = MyAdmob();
 
+  // final bannerController = BannerAdController();
+
+  AppOpenAd? appOpenAd;
+  bool _isShowingAd = false;
+  String adUnitId = '0';
   Logger logger = Logger();
 
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
-    // getInfo();
+    adUnitId = MyAdmob.getOpenAdId()!;
+
+    logger.v("adUnitId => $adUnitId ");
+
+    await loadAd();
+
     logger.i("ADMOB CONTROLLER STARTED");
   }
 
   void onClose() {
-    bannerController.dispose();
+    // bannerController.dispose();
   }
 
-  getAdMobAppId() {
+  /// Load an AppOpenAd.
+  Future<void> loadAd() async {
+    logger.v("ADMOB LOADED 😍😍----------*******");
+    logger.v("adUnitId => $adUnitId");
+    AppOpenAd.load(
+      adUnitId: adUnitId,
+      orientation: AppOpenAd.orientationPortrait,
+      request: AdRequest(),
+      adLoadCallback: AppOpenAdLoadCallback(
+        onAdLoaded: (ad) {
+          appOpenAd = ad;
+        },
+        onAdFailedToLoad: (error) {
+          print('AppOpenAd failed to load: $error');
+
+          // Handle the error.
+        },
+      ),
+    );
+  }
+
+  /// Whether an ad is available to be shown.
+  bool get isAdAvailable {
+    return appOpenAd != null;
+  }
+
+  void showAdIfAvailable() {
+    logger.v("SHOWIFAVAIBLABLE 😍😍");
+    if (!isAdAvailable) {
+      print('Tried to show ad before available.');
+      loadAd();
+      return;
+    }
+    if (_isShowingAd) {
+      print('Tried to show ad while already showing an ad.');
+      return;
+    }
+    // Set the fullScreenContentCallback and show the ad.
+    appOpenAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (ad) {
+        _isShowingAd = true;
+        print('$ad onAdShowedFullScreenContent');
+      },
+      onAdFailedToShowFullScreenContent: (ad, error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        _isShowingAd = false;
+        ad.dispose();
+        appOpenAd = null;
+      },
+      onAdDismissedFullScreenContent: (ad) {
+        print('$ad onAdDismissedFullScreenContent');
+        _isShowingAd = false;
+        ad.dispose();
+        appOpenAd = null;
+        loadAd();
+      },
+    );
+    appOpenAd!.show();
+  }
+
+  String? getAdMobAppId() {
     if (Platform.isIOS) {
       return TEST ? MyAdmob.TEST_app_id_ios : MyAdmob.PROD_app_id_ios;
     } else if (Platform.isAndroid) {
@@ -40,7 +109,7 @@ class AdmobController extends GetxController {
     return null;
   }
 
-  getBannerAdId() {
+  String? getBannerAdId() {
     if (Platform.isIOS) {
       // return ;
       return TEST ? MyAdmob.TEST_banner_id_ios : MyAdmob.PROD_banner_id_ios;
@@ -53,20 +122,20 @@ class AdmobController extends GetxController {
     return null;
   }
 
-  getInterstitialAdId() {
-    if (Platform.isIOS) {
-      return TEST
-          ? MyAdmob.TEST_interstitial_id_ios
-          : MyAdmob.PROD_interstitial_id_ios;
-    } else if (Platform.isAndroid) {
-      return TEST
-          ? MyAdmob.TEST_interstitial_id_android
-          : MyAdmob.PROD_interstitial_id_android;
-    }
-    return null;
-  }
+  // String? getInterstitialAdId() {
+  //   if (Platform.isIOS) {
+  //     return TEST
+  //         ? MyAdmob.TEST_interstitial_id_ios
+  //         : MyAdmob.PROD_interstitial_id_ios;
+  //   } else if (Platform.isAndroid) {
+  //     return TEST
+  //         ? MyAdmob.TEST_interstitial_id_android
+  //         : MyAdmob.PROD_interstitial_id_android;
+  //   }
+  //   return null;
+  // }
 
-  getOpenAdId() {
+  String? getOpenAdId() {
     if (Platform.isIOS) {
       return TEST ? MyAdmob.TEST_open_ad_id_ios : MyAdmob.PROD_open_ad_id_ios;
     } else if (Platform.isAndroid) {
@@ -77,53 +146,47 @@ class AdmobController extends GetxController {
     return null;
   }
 
-  loadInterstitial() {
-    interstitialAd = InterstitialAd(unitId: getInterstitialAdId());
-    // myInterstitial = InterstitialAd(
-    //   adUnitId: getInterstitialAdId(),
-    //   request: AdRequest(),
-    //   listener: AdListener(),
-    // );
+  // loadInterstitial() {
+  //   interstitialAd = InterstitialAd(unitId: getInterstitialAdId());
+  //   logger.i("interstitial loading..");
 
-    logger.i("interstitial loading..");
+  //   interstitialAd.load();
+  // }
 
-    interstitialAd.load();
-  }
+  // showInterstitial() async {
+  //   if (interstitialAd.isLoaded) {
+  //     logger.i("interstitial is loaded");
+  //     interstitialAd.show();
+  //   }
+  // }
 
-  showInterstitial() async {
-    if (interstitialAd.isLoaded) {
-      logger.i("interstitial is loaded");
-      interstitialAd.show();
-    }
-  }
+  // loadOpenad() {
+  //   appOpenAd = AppOpenAd(timeout: Duration(minutes: 30) );
 
-  loadOpenad() {
-    appOpenAd = AppOpenAd(timeout: Duration(minutes: 30));
+  //   appOpenAd.load(orientation: AppOpenAd.ORIENTATION_PORTRAIT);
+  // }
 
-    appOpenAd.load(orientation: AppOpenAd.ORIENTATION_PORTRAIT);
-  }
+  // showAppOpen() async {
+  //   if (!appOpenAd.isAvailable) await appOpenAd.load();
+  //   if (appOpenAd.isAvailable) {
+  //     await appOpenAd.show();
+  //     // Load a new ad right after the other one was closed
+  //     appOpenAd.load();
+  //   }
+  // }
 
-  showAppOpen() async {
-    if (!appOpenAd.isAvailable) await appOpenAd.load();
-    if (appOpenAd.isAvailable) {
-      await appOpenAd.show();
-      // Load a new ad right after the other one was closed
-      appOpenAd.load();
-    }
-  }
-
-  showBanner() async {
-    bannerController.onEvent.listen((e) {
-      final event = e.keys.first;
-      // final info = e.values.first;
-      switch (event) {
-        case BannerAdEvent.loaded:
-          // setState(() => _bannerAdHeight = (info as int)?.toDouble());
-          break;
-        default:
-          break;
-      }
-    });
-    bannerController.load();
-  }
+  // showBanner() async {
+  //   bannerController.onEvent.listen((e) {
+  //     final event = e.keys.first;
+  //     // final info = e.values.first;
+  //     switch (event) {
+  //       case BannerAdEvent.loaded:
+  //         // setState(() => _bannerAdHeight = (info as int)?.toDouble());
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   });
+  //   bannerController.load();
+  // }
 }
